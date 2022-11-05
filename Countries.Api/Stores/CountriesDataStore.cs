3ec1a4a5c075146d5075897cd7cc5cfed6ee12ad
william.walkley <william.walkley@re-leased.com>
2022-11-06 12:16:03 +1300

    Introduced CountriesDataStore.cs. A place to store data whilst building initial application functions

---
+++ Countries.Api/Stores/CountriesDataStore.cs
@@ -0,0 +1,26 @@
+﻿using Countries.Api.Models;
+
+namespace Countries.Api.Stores;
+
+public sealed class CountriesDataStore
+{
+    public List<Country> Countries { get; set; }
+
+    public CountriesDataStore()
+    {
+        Countries = GetCitiesData();
+    }
+
+    /// <summary>
+    /// Initial data store to test without database.
+    /// </summary>
+    private static List<Country> GetCitiesData()
+    {
+        return new List<Country>
+        {
+            new Country { Id = 1, Name = "New Zealand", Capital = "Wellington", Population = 5131480 },
+            new Country { Id = 2, Name = "Australia", Capital = "Canberra", Population = 25890773 },
+            new Country { Id = 3, Name = "England", Capital = "London", Population = 56489800 }
+        };
+    }
+}
