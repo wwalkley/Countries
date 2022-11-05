4c830205f7f1308ca73eed2af0a636b0cefe3e4a
william.walkley <william.walkley@re-leased.com>
2022-11-06 11:58:18 +1300

    Add Country.cs Model

---
+++ Countries.Api/Models/Country.cs
@@ -0,0 +1,9 @@
+﻿namespace Countries.Api.Models;
+
+public sealed class Country
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Capital { get; set; }
+    public int Population { get; set; }
+}
