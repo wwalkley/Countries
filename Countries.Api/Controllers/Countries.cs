4cacef48e8e1307edd16003693dceb65a2262622
william.walkley <william.walkley@re-leased.com>
2022-11-06 11:37:57 +1300

    Add Countries controller with single end point and dummy data

---
+++ Countries.Api/Controllers/Countries.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Countries.Api.Controllers;
+
+[ApiController]
+[Route("api/countries")]
+public sealed class Countries : ControllerBase
+{
+    /// <summary>
+    /// Returns a list of Countries. 
+    /// </summary>
+    [HttpGet]
+    public JsonResult GetCountries()
+    {
+        return new JsonResult(
+            new List<object>
+            {
+                new { Id = 1, Name = "New Zealand" },
+                new { Id = 2, Name = "Australia" }
+            });
+    }
+}
