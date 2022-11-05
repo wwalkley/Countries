16df4dce1505bc77f37b8b4aa93669d5cd215633
william.walkley <william.walkley@re-leased.com>
2022-11-06 12:31:33 +1300

    Added Get Country by Id to Countries end point

--- Countries.Api/Controllers/Countries.cs
+++ Countries.Api/Controllers/Countries.cs
@@ -11,11 +11,23 @@ public sealed class Countries : ControllerBase
     private CountriesDataStore DummyStore { get; set; } = new();
 
     /// <summary>
-    /// Returns a list of Countries. 
+    /// Returns a list of <see cref="Country"/>
     /// </summary>
     [HttpGet]
     public JsonResult GetCountries()
     {
         return new JsonResult(DummyStore.Countries);
+    }
+
+    /// <summary>
+    /// Returns a singular <see cref="Country"/> based on provided Id.
+    /// </summary>
+    /// <param name="id">An Id for a specific Country</param>
+    [HttpGet("{id}")]
+    public JsonResult GetCountry(int id)
+    {
+        var country = DummyStore.Countries.FirstOrDefault(c => c.Id == id);
+
+        return country == default ? new JsonResult(new { }) : new JsonResult(country);
     }
 }
