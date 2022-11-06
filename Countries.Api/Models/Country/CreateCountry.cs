2bd1f2e34909f635c59a9ded8b4112a198151555
william.walkley <william.walkley@re-leased.com>
2022-11-06 16:04:35 +1300

    Made Create Dto for Country

---
+++ Countries.Api/Models/Country/CreateCountry.cs
@@ -0,0 +1,8 @@
+﻿namespace Countries.Api.Models.Country;
+
+public sealed class CreateCountry
+{
+    public string Name { get; set; }
+    public string Capital { get; set; }
+    public int Population { get; set; }
+}
