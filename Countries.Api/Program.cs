43a0eaa5a4b1bcaf084f898fbeaf856a8acb9894
william.walkley <william.walkley@re-leased.com>
2022-11-12 14:30:09 +1300

    Added Serilog and made it the logger

--- Countries.Api/Program.cs
+++ Countries.Api/Program.cs
@@ -1,10 +1,21 @@
+using Serilog;
+
+Log.Logger = new LoggerConfiguration( )
+    .MinimumLevel.Debug( )
+    .WriteTo.Console( )
+    .WriteTo.File( "logs/log.txt", rollingInterval: RollingInterval.Day )
+    .CreateLogger( );
+
 var builder = WebApplication.CreateBuilder( args );
+
+builder.Host.UseSerilog( );
 
 // Add services to the container.
 builder.Services
     .AddControllers( )
     .AddNewtonsoftJson( )
     .AddXmlDataContractSerializerFormatters( );
+
 
 builder.Services.AddEndpointsApiExplorer( );
 builder.Services.AddSwaggerGen( );
